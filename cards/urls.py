470240c8a220fd30600029af882dc329484bc4ee
Stephen Foley <chieffoley@gmail.com>
2016-05-23 20:49:50 -0400

    more static fix

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import url
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -8,7 +6,4 @@ urlpatterns = [
    url(r'^$', views.IndexView.as_view(), name='index'),
     #ex /polls/5
    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-   url(r'^admin/', admin.site.urls)
 ]
-
-urlpatterns += staticfiles_urlpatterns()
