a4a0920a5c2432fea4d498df5b727c55cda52dea
Stephen Foley <chieffoley@gmail.com>
2016-05-23 20:44:56 -0400

    more static fix

--- cards/urls.py
+++ cards/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
    url(r'^$', views.IndexView.as_view(), name='index'),
     #ex /polls/5
    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+   url(r'^admin/', admin.site.urls)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
